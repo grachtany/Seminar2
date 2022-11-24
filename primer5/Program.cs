6b7a795cae7f478d7127d5a3f894815f5727708d
tatangracheva <grachtany@yandex.ru>
2022-11-24 21:58:51 +0300

    проверка квадрата числа

---
+++ primer5/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.Write("Введите число a - ");
+int numberA = int.Parse(Console.ReadLine());
+
+Console.Write("Введите число b - ");
+int numberB = int.Parse(Console.ReadLine());
+
+if (numberB == numberA*numberA) 
+    Console.Write ($"{numberB}  является квадратом {numberA}");
+else 
+    Console.Write ($"{numberB}  не является квадратом {numberA}");
