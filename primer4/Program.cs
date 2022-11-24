c5ac4cbb271ae9bea55432d1312174f1065349c8
tatangracheva <grachtany@yandex.ru>
2022-11-24 21:45:01 +0300

    сделали кратность чисел на 7 и 23

---
+++ primer4/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+
+Console.Clear();
+Console.Write("Введите число a - ");
+
+int numberA = int.Parse(Console.ReadLine());
+
+int ostatok1 = numberA%7;
+int ostatok2 = numberA%23;
+if ((ostatok1 ==0) && (ostatok2 ==0))
+    Console.Write ($"{numberA}  Кратно 7 и 23");
+else 
+    Console.Write ($"{numberA}  не кратно 7 и 23");
+
+
+
