8e2cda2dd2ced7e95ef76e2bd0fb4993b1fde9c5
tatangracheva <grachtany@yandex.ru>
2022-11-24 20:38:50 +0300

    Пример с трехзначными числами

---
+++ primer2/Program.cs
@@ -0,0 +1,16 @@
+﻿int numberA = new Random().Next(10, 100)  ;
+Console.WriteLine($"Случайное число {numberA}");
+ int n1 = numberA/10;
+ Console.WriteLine ($"Первая цифра {n1}");
+ int n2 = numberA%10;
+  Console.WriteLine ($"Вторая цифра {n2}");
+if (n1>n2) Console.WriteLine($"Максимальное из двух чисел равно {n1}" );
+if (n1<n2) Console.WriteLine($"Максимальное из двух чисел равно {n2}");
+
+int max = n1;
+
+if (n2>max)
+{max=n2;
+};
+Console.WriteLine (max);
+
