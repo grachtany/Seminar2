575c832cde1b90a64f757f7f260e7d28ca477dc5
tatangracheva <grachtany@yandex.ru>
2022-11-24 21:20:12 +0300

    сделали кратность чисел

---
+++ primer3/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+Console.Write("Введите число a - ");
+int numberA = int.Parse(Console.ReadLine());
+Console.Write("Введите число b - ");
+int numberB = int.Parse(Console.ReadLine());
+int ostatok = numberA%numberB;
+if (ostatok ==0) Console.Write ($"{numberA}  Кратно {numberB}");
+else 
+{Console.Write ($"{numberA}  не кратно {numberB}, остаток {ostatok}");}
